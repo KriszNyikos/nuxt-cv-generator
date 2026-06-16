c782f46cf050b9dd306878c25cb425bb7f8e3260
Krisztian Nyikos <krisz.nyikos@gmail.com>
2026-06-16 08:40:13 +0200

    feat: create CvAbout component for displaying personal information

---
+++ app/components/CvAbout.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { CvContent } from '@/types'
+
+const { about } = defineProps<{
+  about: CvContent['about']
+}>()
+</script>
+
+<template>
+  <div>
+    <h1>About me</h1>
+    <p>{{ about }}</p>
+  </div>
+</template>
