49ba1979cb74b1314584657597d47522cbf97f37
Krisztian Nyikos <krisz.nyikos@gmail.com>
2026-06-19 11:45:14 +0200

    feat: add navigation menu items for GitHub and LinkedIn in CvHeader component

--- app/components/CvHeader.vue
+++ app/components/CvHeader.vue
@@ -1,15 +1,21 @@
 <script setup lang="ts">
 import type { CvContent } from '@/types'
+import type { NavigationMenuItem } from '@nuxt/ui'
 
 const props = defineProps<{ name: string, jobTitle: string, contact: CvContent['contact'] }>()
+const items = computed<NavigationMenuItem[]>(() => [
+  { label: 'Github', to: props.contact.github, target: '_blank', icon: 'i-simple-icons-github' },
+  { label: 'Linkedin', to: props.contact.linkedin, target: '_blank', icon: 'i-simple-icons-linkedin' }
+])
 </script>
 
 <template>
   <UHeader
-    :toggle="false"
     :ui="{
-      root: 'border-none bg-transparent'
+      root: 'border-none bg-transparent',
+      toggle: 'lg:hidden print:hidden'
     }"
+    mode="slideover"
   >
     <template #left>
       <div class="flex flex-col gap-1">
@@ -31,21 +37,32 @@ const props = defineProps<{ name: string, jobTitle: string, contact: CvContent['
         to="/hu"
         class="print:hidden"
       >HU</ULink>
-      <UButton
-        color="neutral"
-        variant="ghost"
-        :href="props.contact.github"
-        target="_blank"
-        icon="i-simple-icons-github"
-        aria-label="GitHub"
-      />
-      <UButton
-        color="neutral"
-        variant="ghost"
-        :href="props.contact.linkedin"
-        target="_blank"
-        icon="i-simple-icons-linkedin"
-        aria-label="LinkedIn"
+      <div class="hidden lg:flex print:flex items-center gap-1">
+        <UButton
+          color="neutral"
+          variant="ghost"
+          :href="props.contact.github"
+          target="_blank"
+          icon="i-simple-icons-github"
+          aria-label="GitHub"
+        />
+        <UButton
+          color="neutral"
+          variant="ghost"
+          :href="props.contact.linkedin"
+          target="_blank"
+          icon="i-simple-icons-linkedin"
+          aria-label="LinkedIn"
+        />
+      </div>
+    </template>
+
+    <template
+      #body
+    >
+      <UNavigationMenu
+        :items="items"
+        orientation="vertical"
       />
     </template>
   </UHeader>
